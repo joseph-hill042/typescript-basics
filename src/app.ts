1288b6097d024232d945f15b9c4a1440be41c29a
Joseph Hill <joseph.hill1@outlook.com>
2022-08-27 17:01:13 -0400

    arrow functions and implicit returns

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello TypeScript!');
+const pizzas = [{ name: 'Pepperoni', toppings: ['pepperoni'] }];
+
+const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+
+const pizza = {
+  name: 'Blazing Inferno',
+  getName() {
+    return this.name;
+  },
+};
+
+console.log(pizza.getName());
