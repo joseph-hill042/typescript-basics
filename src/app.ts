6d76208bbdac502ffc86856062f3dd35bf01c752
Joseph Hill <joseph.hill1@outlook.com>
2022-08-27 17:29:12 -0400

    object literal improvements

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,47 @@
 const pizzas = [{ name: 'Pepperoni', toppings: ['pepperoni'] }];
 
-const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+// ---> ARROW FUNCTIONS AND IMPLICIT RETURN <---
+{
+  const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
 
-console.log(mappedPizzas);
+  console.log(mappedPizzas);
 
-const pizza = {
-  name: 'Blazing Inferno',
-  getName() {
-    return this.name;
-  },
-};
+  const pizza = {
+    name: 'Blazing Inferno',
+    getName() {
+      return this.name;
+    },
+  };
 
-console.log(pizza.getName());
-
-function multiply(x: number, y = 25) {
-  return x * y;
+  console.log(pizza.getName());
 }
+// ---> DEFAULT FUNCTION PARAMETERS <---
+{
+  const multiply = (x: number, y = 25) => x * y;
 
-console.log(multiply(5));
-console.log(multiply(5, 10));
+  console.log(multiply(5));
+  console.log(multiply(5, 10));
+}
+// ---> OBJECT LITERAL IMPROVEMENTS <---
+{
+  const pizza = {
+    name: 'Pepperoni',
+    price: 15,
+    getName() {
+      return this.name;
+    },
+  };
+
+  console.log(pizza.getName());
+
+  const toppings = ['pepperoni'];
+
+  const createOrder = (pizza: { name: string; price: number }, toppings: string[]) => {
+    return {
+      pizza,
+      toppings,
+    };
+  };
+
+  console.log(createOrder(pizza, toppings));
+}
